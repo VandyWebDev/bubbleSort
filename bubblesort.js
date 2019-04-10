1247fe81aae50a5934ae7414f5ea08af042a0f8f
VandyWebDev <39500384+VandyWebDev@users.noreply.github.com>
2019-04-10 16:57:32 -0400

    bubblesort

--- bubblesort.js
+++ bubblesort.js
@@ -1,14 +1,15 @@
 function bubbleSort(array) {
-	let arrLength = array.length - 1;
-	let midPoint = Math.round(arrLength / 2);
-	for (let j = arrLength; j >= midPoint; j--) {
-		for (let i = 0; i < j; i++) {
-			if (array[i] > array[i + 1]) {
-				let temp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = temp;
-			}
-		}
-	}
-	return array;
+  let arrLength = array.length - 1;
+  for (let j = arrLength; j >= 0; j--) {
+    let swapped = false;
+    for (let i = 0; i < j; i++) {
+      if (array[i] > array[i + 1]) {
+        [array[i], array[i + 1]] = [array[i + 1], array[i]];
+        swapped = true;
+      }
+    }
+    if (!swapped) break;
+  }
+  return array;
 }
+console.log(bubbleSort([2, 8, 1, 3, 6, 0, 30, 19, -1, 29]));
